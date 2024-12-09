3e91aa872846e2ed3cb0156d3b31b1c227213ab9
Arnaud Sok <arnaudsokprivate@gmail.com>
2024-12-09 18:31:23 +0100

    Début fonction image_to_graphe, petit pause

--- src/img_manager.py
+++ src/img_manager.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from graph import Graphe
 
 def load_image(image_path):
     """
@@ -10,3 +11,32 @@ def load_image(image_path):
     image = Image.open(image_path).convert('L')  # Conversion en niveaux de gris
     image_array = np.array(image)
     return image_array
+
+
+def image_to_graphe(image_array):
+
+    graphe = Graphe()
+
+    # Ajout des sommets (ce sont les pixels de l'image)
+    for i in range (image_array.shape[0]):
+        for j in range (image_array.shape[1]):
+            indivTime = image_array[i,j]
+            graphe.ajouter_sommet(indivTime)
+
+
+
+    # Ajout des arêtes (ce sont les différences d'intensités entre deux arêtes voisines)
+
+    for i in range (image_array[0]):
+        for j in range (image_array[1]):
+
+            source = i * image_array.shape[1] + j
+
+            
+
+
+
+
+           
+
+
